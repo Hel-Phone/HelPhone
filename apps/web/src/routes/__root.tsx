b6f11451a27aea4efd8eaa37ba61fe6220587447
CAESAR MCMXCVII <rubelu2017@gmail.com>
2026-05-27 10:15:34 +0100

    done

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "sonner"
 import { ThemeProvider } from "../ui/theme-provider"
+import { WalletProvider } from "../app/providers"
 import appCss from "@workspace/ui/globals.css?url"
 
 const queryClient = new QueryClient({
@@ -178,8 +179,10 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       <body>
         <QueryClientProvider client={queryClient}>
           <ThemeProvider>
-            {children}
-            <Toaster richColors position="bottom-right" />
+            <WalletProvider>
+              {children}
+              <Toaster richColors position="bottom-right" />
+            </WalletProvider>
           </ThemeProvider>
         </QueryClientProvider>
         <Scripts />
